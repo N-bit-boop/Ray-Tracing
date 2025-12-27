e4503a078954c907e8bf6ef8bf3efe4184cb6f39
niran <niranjanjaishankar07@gmail.com>
2025-12-27 18:49:43 -0500

    Seperated sphere logic, created hittable abastraction

---
+++ sphere.hpp
@@ -0,0 +1,48 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.hpp"
+#include "vec.hpp"
+
+
+class sphere : public hittable{
+    public:
+        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)){}
+
+
+        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override{
+            vec3 oc  = center - r.origin(); // C - P
+            auto a = r.direction().length_squared();
+            auto h = dot(r.direction(),oc);
+            auto c =  oc.length_squared() - radius*radius;
+            auto discriminant = h*h -a*c;
+
+            if(discriminant < 0){
+                return false;
+            }
+            auto sqrd = std::sqrt(discriminant);
+
+            //Find the nearest root in min < t < max 
+            auto root =  (h - sqrd) / a;
+            if(root <= ray_tmin || ray_tmax <= root){
+                root  =  (h + sqrd) / a;
+                if(root <= ray_tmin || ray_tmax <= root){
+                    return false;
+                }
+            }
+
+            rec.t = root;
+            rec.p = r.at(rec.t);
+            rec.normal = (rec.p - center ) / radius; //Divided for the uniot lenght
+
+            return true;
+
+        }
+
+    private:
+    point3 center;
+    double radius;
+};
+
+
+#endif 
