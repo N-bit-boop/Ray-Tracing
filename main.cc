d93a697f427d5e6eff2ef63b09728d85c17bd377
niran <niranjanjaishankar07@gmail.com>
2025-12-27 18:03:18 -0500

    Red sphere on gradient

--- main.cc
+++ main.cc
@@ -4,7 +4,21 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc  = center - r.origin(); // C - P
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 *dot(r.direction(), oc);
+    auto c =  dot(oc,oc) - radius*radius;
+    auto discriminant = b*b -4*a*c;
+    return(discriminant >=0); // Return if the ray touches the sphere or no 
+}
+
+
 color ray_color(const ray& r ){
+    if(hit_sphere(point3(0,0,-1),0.5,r)){
+        return color(1,0,0); //For the rays that hit the sphere --> trun red 
+    }
+
     vec3 unit_direction  = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1 );
     return (1.0 - a)*color(1.0,1.0, 1.0) + a*color(0.5,0.7,1.0);
