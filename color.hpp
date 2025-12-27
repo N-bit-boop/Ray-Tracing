04b56dece0136ad69bc4cf1542faf6191a62caee
niran <niranjanjaishankar07@gmail.com>
2025-12-27 13:23:40 -0500

    Chapter 3 complete

---
+++ color.hpp
@@ -0,0 +1,28 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <iostream>
+
+#include "vec.hpp"
+
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color){
+    auto r  = pixel_color.x();
+    auto g  = pixel_color.y();
+    auto b  = pixel_color.z();
+
+    //Translate [0,1 ] comp values to the byte range [0,255]
+
+    int rbyte = int(255.999 * r );
+    int gbyte = int(255.999 * g );
+    int bbyte = int(255.999 * b );
+
+    //Write out pixel colour componenrs 
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+
+
+#endif 
