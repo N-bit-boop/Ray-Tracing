ec0405d41aed59289a7eeb9f1f7255561e15dfb1
niran <niranjanjaishankar07@gmail.com>
2025-12-29 18:55:46 -0500

    adjustable fov lens

--- camera.hpp
+++ camera.hpp
@@ -11,6 +11,11 @@ class camera{
         int samples_per_pixel = 10;
         int max_depth = 10;
 
+        double vfov = 90; //vertical view
+        point3 lookfrom = point3(0,0,0);
+        point3 lookat = point3(0,0,-1);
+        vec3 vup = vec3(0,1,0);
+
         void render(const hittable& world ){
             initialize();
 
@@ -39,6 +44,7 @@ class camera{
         vec3 pixel_delta_u;
         vec3 pixel_delta_v;
         double pixel_samples_scale;
+        vec3 u,v,w; //Camera frame basis vectors 
 
         color ray_color(const ray& r, int depth, const hittable& world) const{
             if (depth <= 0){
@@ -66,24 +72,31 @@ class camera{
             image_height = (image_height < 1) ? 1 : image_height;
 
             pixel_samples_scale = 1.0/ samples_per_pixel;
-            center = point3(0,0,0);
+            center = lookfrom;
 
             //camera 
 
-            auto focal_length = 1.0; //how far the lens is 
-            auto viewport_height = 2.0;
+            auto focal_length = (lookfrom - lookat).length(); //how far the lens is 
+            auto theta = degrees_to_radians(vfov);
+            auto h = std::tan(theta/2);
+            auto viewport_height = 2 * h * focal_length;
             auto viewport_width = viewport_height * (double(image_width)/image_height);
+            
+            // Calc the basis vectors for the camera frame
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup,w));
+            v = cross(w,u);  
 
             //Calc vectors across the hor and ver viewport edges 
-            auto viewport_u = vec3(viewport_width,0,0);
-            auto viewport_v =vec3(0,-viewport_height,0); // As the y directioin is inversed here 
+            auto viewport_u = viewport_width*u; //vector across veiwport hor edge 
+            auto viewport_v =viewport_height * -v; // As the y directioin is inversed here 
 
             //Calc horizontal and vertical delta vectors from pixel to pixel
             pixel_delta_u = viewport_u / image_width; //Width of one pixel 
             pixel_delta_v = viewport_v / image_height; //Height of one pixel
 
             //clac the locatioin of the upper left pixel
-            auto viewport_upper_left  = center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+            auto viewport_upper_left  = center - (focal_length *w ) - viewport_u/2 -viewport_v/2;
             //Above: Move viewport forward from capera go left half width and up hadl the heihjt
             pixel00_loc =  viewport_upper_left + 0.5 *(pixel_delta_u + pixel_delta_v);
 
